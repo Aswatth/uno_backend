ef87e5d55d34fd7cd969a63865f146a64a1cfd43
Aswatth <aswatth56@gmail.com>
2025-03-18 15:42:11 -0400

    Create PlayerServiceTest.java
    Added unit tests for player service

---
+++ src/test/java/com/example/uno/services/PlayerServiceTest.java
@@ -0,0 +1,102 @@
+package com.example.uno.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.uno.models.ConnectionData;
+import com.example.uno.models.Player;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class PlayerServiceTest {
+
+  @Autowired
+  IPlayerService playerService;
+
+  @Test
+  public void contextLoad() {
+    assertThat(playerService).isNotNull();
+  }
+
+  @Test
+  public void testAddPlayer() {
+    String sessionId = "123";
+    String playerName = "testPlayer";
+    ConnectionData connectionData = new ConnectionData("0.0.0.0", 12345);
+
+    playerService.addPlayer(sessionId, playerName, connectionData);
+
+    Player player = playerService.getPlayer("123");
+
+    assertThat(player).isNotNull();
+
+    assertThat(player.getSessionId()).isEqualTo(sessionId);
+    assertThat(player.getName()).isEqualTo(playerName);
+
+    assertThat(player.getConnectionData().ipAddress()).isEqualTo(connectionData.ipAddress());
+    assertThat(player.getConnectionData().port()).isEqualTo(connectionData.port());
+
+  }
+
+  @Test
+  public void testRemovePlayer() {
+    String sessionId = "123";
+    String playerName = "testPlayer";
+    ConnectionData connectionData = new ConnectionData("0.0.0.0", 12345);
+
+    playerService.addPlayer(sessionId, playerName, connectionData);
+
+    playerService.removePlayer(sessionId);
+
+    Player player = playerService.getPlayer("123");
+
+    assertThat(player).isNull();
+  }
+
+  @Test
+  public void testGetPlayer() {
+    String sessionId = "123";
+    String playerName = "testPlayer";
+    ConnectionData connectionData = new ConnectionData("0.0.0.0", 12345);
+
+    playerService.addPlayer(sessionId, playerName, connectionData);
+
+    Player player = playerService.getPlayer("123");
+
+    assertThat(player.getSessionId()).isEqualTo(sessionId);
+    assertThat(player.getName()).isEqualTo(playerName);
+
+    assertThat(player.getConnectionData().ipAddress()).isEqualTo(connectionData.ipAddress());
+    assertThat(player.getConnectionData().port()).isEqualTo(connectionData.port());
+  }
+
+  @Test
+  public void testGetPlayerFailure() {
+    Player player = playerService.getPlayer("123");
+
+    assertThat(player).isNull();
+  }
+
+  @Test
+  public void testGetPlayerSessionIdList() {
+    int count = 5;
+    for (int i = 1; i <= count; ++i) {
+      String sessionId = Integer.toString(i);
+      String playerName = "testPlayer" + i;
+      ConnectionData connectionData = new ConnectionData("0.0.0.0", 12345);
+
+      playerService.addPlayer(sessionId, playerName, connectionData);
+    }
+
+    List<String> sessionIdList = playerService.getPlayerSessionIdList();
+
+    assertThat(sessionIdList.size()).isEqualTo(count);
+    for (int i = 1; i <= count; ++i) {
+      assertThat(sessionIdList.get(i - 1)).isEqualTo(Integer.toString(i));
+    }
+  }
+
+}

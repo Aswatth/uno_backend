6904966cf35a42f97df530b61a7f56aef7473b80
Aswatth <aswatth56@gmail.com>
2025-03-20 10:17:58 -0400

    Create PlayerModelTest.java
    Unit tests for player model class.

---
+++ src/test/java/com/example/uno/models/PlayerModelTest.java
@@ -0,0 +1,62 @@
+package com.example.uno.models;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PlayerModelTest {
+
+  private final String sessionId = "123";
+  private final String playerName = "testPlayer";
+  private final ConnectionData connectionData = new ConnectionData("0.0.0.0", 1234);
+  private Player player;
+
+  @BeforeEach
+  public void setup() {
+    player = new Player(sessionId, playerName, connectionData);
+  }
+
+  @Test
+  public void testGetSessionId() {
+    assertThat(player.getSessionId()).isEqualTo(sessionId);
+  }
+
+  @Test
+  public void testSetSessionId() {
+    String newSessionId = "234";
+    player.setSessionId(newSessionId);
+
+    assertThat(player.getSessionId()).isEqualTo(newSessionId);
+  }
+
+  @Test
+  public void testGetName() {
+    assertThat(player.getName()).isEqualTo(playerName);
+  }
+
+  @Test
+  public void testSetName() {
+    String newPlayerName = "newName";
+    player.setName(newPlayerName);
+
+    assertThat(player.getName()).isEqualTo(newPlayerName);
+  }
+
+  @Test
+  public void testGetConnectionData() {
+    assertThat(player.getConnectionData()).isEqualTo(connectionData);
+  }
+
+  @Test
+  public void testSetConnectionData() {
+    ConnectionData newConnectionData = new ConnectionData("0.0.0.0", 12345);
+    player.setConnectionData(newConnectionData);
+
+    assertThat(player.getConnectionData()).isEqualTo(newConnectionData);
+
+  }
+
+}

2471963ba5c2a133dc424d2ed0f29f6b17b7aaa6
Aswatth <aswatth56@gmail.com>
2025-03-29 08:19:36 -0400

    Updated controller tests
    - Refactoring based on SonarQube recommendations.

--- src/test/java/com/example/uno/controllers/ITGameControllerTest.java
+++ src/test/java/com/example/uno/controllers/ITGameControllerTest.java
@@ -7,27 +7,23 @@ import static org.awaitility.Awaitility.await;
 import com.example.uno.services.GameService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
@@ -37,10 +33,10 @@ import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
-public class ITGameControllerTest {
+class ITGameControllerTest {
 
   @LocalServerPort
-  int PORT;
+  int port;
 
   static final String WEBSOCKET_URI = "ws://localhost:$PORT/uno";
   BlockingQueue<String> blockingQueue;
@@ -66,31 +62,30 @@ public class ITGameControllerTest {
   }
 
   @BeforeEach
-  public void setup() {
+  void setup() {
     blockingQueue = new LinkedBlockingDeque<>();
     stompClient = new WebSocketStompClient(new StandardWebSocketClient());
   }
 
   @Test
-  public void contextLoad() {
+  void contextLoad() {
     assertThat(gameController).isNotNull();
   }
 
   @Test
-  public void testCreateGame() throws Exception {
+  void testCreateGame() throws Exception {
 
     String gameName = "testGame";
     int minPlayers = 2;
     String gameId = "123";
 
-    Map<String, Object> payload = new HashMap<>();
-    payload.put("gameName", gameName);
-    payload.put("minPlayers", minPlayers);
+    Map<String, Object> payload = Map.ofEntries(Map.entry("gameName", gameName),
+        Map.entry("minPlayers", minPlayers));
 
     Mockito.when(gameService.createGame(gameName, minPlayers)).thenReturn(gameId);
 
     StompSession session = stompClient
-        .connectAsync(WEBSOCKET_URI.replace("$PORT", Integer.toString(PORT)),
+        .connectAsync(WEBSOCKET_URI.replace("$PORT", Integer.toString(port)),
             new StompSessionHandlerAdapter() {
             })
         .get(1, SECONDS);
@@ -98,32 +93,25 @@ public class ITGameControllerTest {
 
     session.send("/app/game", new ObjectMapper().writeValueAsBytes(payload));
 
+    Mockito.verify(gameService).createGame(gameName, minPlayers);
+
     await().atMost(1, SECONDS).untilAsserted(() ->
         assertThat(blockingQueue.poll()).isEqualTo(gameId)
     );
   }
 
   @Test
-  public void testBrowseGames() throws Exception {
-    List<Map<String, Object>> gameList = new ArrayList<>();
-    gameList.add(new HashMap<>() {{
-      put("gameName", "testGame1");
-      put("currentPlayers", new ArrayList<>() {{
-        add("testPlayer1");
-      }});
-    }});
-    gameList.add(new HashMap<>() {{
-      put("gameName", "testGame2");
-      put("currentPlayers", new ArrayList<>() {{
-        add("testPlayer2");
-        add("testPlayer3");
-      }});
-    }});
+  void testBrowseGames() throws Exception {
+    List<Map<String, Object>> gameList = Arrays.asList(Map.ofEntries(
+        Map.entry("gameName", "testGame1"),
+        Map.entry("currentPlayers", Collections.singleton("testPlayer1"))), Map.ofEntries(
+        Map.entry("gameName", "testGame2"),
+        Map.entry("currentPlayers", Arrays.asList("testPlayer2", "testPlayer3"))));
 
     Mockito.when(gameService.browseGames()).thenReturn(gameList);
 
     StompSession session = stompClient
-        .connectAsync(WEBSOCKET_URI.replace("$PORT", Integer.toString(PORT)),
+        .connectAsync(WEBSOCKET_URI.replace("$PORT", Integer.toString(port)),
             new StompSessionHandlerAdapter() {
             })
         .get(1, SECONDS);
@@ -131,23 +119,25 @@ public class ITGameControllerTest {
 
     session.send("/app/browse-games", new ObjectMapper().writeValueAsBytes(""));
 
+    Mockito.verify(gameService).browseGames();
+
     await().atMost(1, SECONDS).untilAsserted(() ->
-        assertThat(blockingQueue.poll().getBytes()).isEqualTo(
+        assertThat(Objects.requireNonNull(blockingQueue.poll()).getBytes()).isEqualTo(
             new ObjectMapper().writeValueAsBytes(gameList))
     );
   }
 
   @Test
-  public void testJoinGame() throws Exception {
+  void testJoinGame() throws Exception {
     String gameId = "123";
 
     StompSession session = stompClient
-        .connectAsync(WEBSOCKET_URI.replace("$PORT", Integer.toString(PORT)),
+        .connectAsync(WEBSOCKET_URI.replace("$PORT", Integer.toString(port)),
             new StompSessionHandlerAdapter() {
             })
         .get(1, SECONDS);
 
-    Mockito.doNothing().when(gameService).joinGame(Mockito.eq(gameId), Mockito.anyString());
+    Mockito.doNothing().when(gameService).joinGame(gameId, Mockito.anyString());
 
     session.subscribe("/topic/join-game/123", new DefaultStompFrameHandler());
 
@@ -158,13 +148,13 @@ public class ITGameControllerTest {
   }
 
   @Test
-  public void testStartGame() throws Exception {
+  void testStartGame() throws Exception {
     String gameId = "123";
 
-    Mockito.doNothing().when(gameService).startGame(Mockito.eq(gameId));
+    Mockito.doNothing().when(gameService).startGame(gameId);
 
     StompSession session = stompClient
-        .connectAsync(WEBSOCKET_URI.replace("$PORT", Integer.toString(PORT)),
+        .connectAsync(WEBSOCKET_URI.replace("$PORT", Integer.toString(port)),
             new StompSessionHandlerAdapter() {
             })
         .get(1, SECONDS);
@@ -173,6 +163,6 @@ public class ITGameControllerTest {
 
     session.send("/app/game/" + gameId, new ObjectMapper().writeValueAsBytes(""));
 
-    Mockito.verify(gameService, Mockito.timeout(1000)).startGame(Mockito.eq(gameId));
+    Mockito.verify(gameService, Mockito.timeout(1000)).startGame(gameId);
   }
 }
--- src/test/java/com/example/uno/controllers/UTGameControllerTest.java
+++ src/test/java/com/example/uno/controllers/UTGameControllerTest.java
@@ -3,8 +3,8 @@ package com.example.uno.controllers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.uno.services.GameService;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -15,7 +15,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class UTGameControllerTest {
+class UTGameControllerTest {
 
   @Mock
   GameService gameService;
@@ -24,13 +24,14 @@ public class UTGameControllerTest {
   GameController gameController;
 
   @Test
-  public void testCreateGame() {
+  void testCreateGame() {
     String gameName = "testGameName";
     int minPlayers = 2;
     String gameId = "123";
-    Map<String, Object> request = new HashMap<>();
-    request.put("gameName", gameName);
-    request.put("minPlayers", minPlayers);
+    Map<String, Object> request = Map.ofEntries(
+        Map.entry("gameName", gameName),
+        Map.entry("minPlayers", minPlayers)
+    );
 
     Mockito.when(gameService.createGame(gameName, minPlayers)).thenReturn(gameId);
 
@@ -39,7 +40,7 @@ public class UTGameControllerTest {
   }
 
   @Test
-  public void testJoinGame() {
+  void testJoinGame() {
     String gameId = "123";
     String playerSessionId = "p123";
 
@@ -51,29 +52,24 @@ public class UTGameControllerTest {
   }
 
   @Test
-  public void testBrowseGames() {
-    List<Map<String, Object>> gameList = new ArrayList<>();
-    gameList.add(new HashMap<>() {{
-      put("gameName", "testGame1");
-      put("currentPlayers", new ArrayList<>() {{
-        add("testPlayer1");
-      }});
-    }});
-    gameList.add(new HashMap<>() {{
-      put("gameName", "testGame2");
-      put("currentPlayers", new ArrayList<>() {{
-        add("testPlayer2");
-        add("testPlayer3");
-      }});
-    }});
+  void testBrowseGames() {
+    List<Map<String, Object>> gameList = Arrays.asList(Map.ofEntries(
+        Map.entry("gameName", "testGame1"),
+        Map.entry("currentPlayers", Collections.singletonList("testPlayer1"))
+    ), Map.ofEntries(
+        Map.entry("gameName", "testGame2"),
+        Map.entry("currentPlayers", Arrays.asList("testPlayer2", "testPlayer3"))
+    ));
 
     Mockito.when(gameService.browseGames()).thenReturn(gameList);
+
+    Mockito.verify(gameService).browseGames();
 
     assertThat(gameController.browseGames()).isEqualTo(gameList);
   }
 
   @Test
-  public void testStartGame() {
+  void testStartGame() {
 
     String gameId = "123";
 
